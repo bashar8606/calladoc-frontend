faadfcaebcef6bb4a0464b89b0cd1a068c9c7d28
bashar8606 <ahammed.bashar9@gmail.com>
2025-09-15 21:54:46 +0400

    jhgv

--- src/components/GlobalProviders/index.js
+++ src/components/GlobalProviders/index.js
@@ -1,53 +1,62 @@
 "use client";
 import { RecoilRoot } from "recoil";
-import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 import fetcher from "@/utils/fetcher";
 import { SWRConfig } from "swr";
-const GlobalProviders = ({ children }) => {
 
+const GlobalProviders = ({ children }) => {
   function localStorageProvider() {
-    // When initializing, we restore the data from `localStorage` into a map.
     if (typeof window !== "undefined") {
       const map = new Map(JSON.parse(localStorage.getItem("app-cache") || "[]"));
-  
-      // Before unloading the app, we write back all the data into `localStorage`.
+
       window.addEventListener("beforeunload", () => {
         const appCache = JSON.stringify(Array.from(map.entries()));
         localStorage.setItem("app-cache", appCache);
       });
-  
-      // We still use the map for write & read for performance.
+
       return map;
     }
   }
 
-  
-
-
   return (
     <>
-    
-       <SWRConfig
-      value={{
-        revalidateOnFocus: true, // Refreshes the data when the window is refocused
-        dedupingInterval: 10000, // Prevents re-fetching the same data within 10 seconds
-        // revalidateOnFocus: false,
-        revalidateOnReconnect: false,
-        fetcher: (resource, init) => fetcher(resource, init),
-        ...(typeof window !== "undefined" && {
-          provider: localStorageProvider,
-        }),
-      }}
-    >
-      <RecoilRoot>{children}</RecoilRoot>
-    </SWRConfig>
-    
-    <TawkMessengerReact
-                propertyId="685bb3263e441e1910e8f100"
-                widgetId="1iuj37kfh"/>
-                
-                </>
- 
+      <SWRConfig
+        value={{
+          revalidateOnFocus: true,
+          dedupingInterval: 10000,
+          revalidateOnReconnect: false,
+          fetcher: (resource, init) => fetcher(resource, init),
+          ...(typeof window !== "undefined" && {
+            provider: localStorageProvider,
+          }),
+        }}
+      >
+        <RecoilRoot>{children}</RecoilRoot>
+      </SWRConfig>
+
+      <TawkMessengerReact
+        propertyId="685bb3263e441e1910e8f100"
+        widgetId="1iuj37kfh"
+        onBeforeLoad={() => {
+          console.log("Tawk is about to load");
+        }}
+        onStatusChange={(status) => {
+          console.log("Status changed:", status);
+        }}
+        onChatHidden={() => {
+          console.log("Chat hidden");
+        }}
+        onChatMessageSystem={(message) => {
+          console.log("System message:", message);
+        }}
+        onUnreadCountChanged={(count) => {
+          console.log("Unread count changed:", count);
+        }}
+        onLoad={() => {
+          console.log("Tawk widget loaded");
+        }}
+      />
+    </>
   );
 };
 
