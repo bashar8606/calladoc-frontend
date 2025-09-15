5250daa3e19f53e218ecea9fadbdd4477bc80b60
bashar8606 <ahammed.bashar9@gmail.com>
2025-09-15 22:05:38 +0400

    jhgv

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,13 @@ const securityHeaders = [
       key: "Content-Security-Policy",
       value: `
         default-src 'self';
-        script-src 'self' 'unsafe-inline' https://embed.tawk.to https://*.tawk.to;
-        connect-src 'self' https://embed.tawk.to wss://embed.tawk.to https://va.tawk.to https://*.tawk.to;
-        style-src 'self' 'unsafe-inline';
+        script-src 'self' 'unsafe-inline' https://embed.tawk.to;
+        connect-src 'self' https://embed.tawk.to wss://embed.tawk.to https://your-api.com;
+        style-src 'self' 'unsafe-inline' https://embed.tawk.to https://fonts.googleapis.com;
+        style-src-elem 'self' 'unsafe-inline' https://embed.tawk.to https://fonts.googleapis.com;
         img-src 'self' data: blob: https:;
+        font-src 'self' https://fonts.gstatic.com;
+        frame-src https://embed.tawk.to;
       `.replace(/\s{2,}/g, " ").trim(),
     },
   ];
