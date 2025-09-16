f4004c667c787097d04d203e96746f37e15b12fb
bashar8606 <ahammed.bashar9@gmail.com>
2025-09-16 20:22:52 +0400

    ihg

--- src/widgets/PackageWidget/index.js
+++ src/widgets/PackageWidget/index.js
@@ -71,49 +71,50 @@ const PackageWidget = ({ data, slug, ...props }) => {
           </h3>
         </div>
         <Card className="w-full h-full bg-white grid gap-4 md:gap-8 grid-cols-1 md:grid-cols-2 mx-auto overflow-hidden  max-w-2xl relative bg-white/30 border border-[#d4e1ff] shadow-[1px_3px_11px_0px_#dbe6ff] backdrop-blur-[12px] rounded-[36px] p-[30px]">
-          
           <div className="">
-          <div className="md:block   mb-6  justify-center ">
+            <div className="md:block   mb-6  justify-center ">
+              {data?.items?.map((item, i) => {
+                return (
+                  <div
+                    key={i}
+                    className={`border border-[#d4e1ff] p-3 mb-3 bg-[linear-gradient(303deg,#2463eb_65%,#abc2f6_102%)] z-[1] rounded-[20px] relative shadow-[0px_0px_10px_1px_#2463eb0f] ${
+                      i !== data?.items.length - 1 ? "" : ""
+                    }`}
+                  >
+                    <div className="mb-1">
+                      <h3 className="text-sm uppercase tracking-wider font-bold text-[#ffffff] mb-1 text-center">
+                        {item?.type}
+                      </h3>
+                      {item?.description && (
+                        <p className="text-white/65 text-sm text-center">
+                          {item?.description}
+                        </p>
+                      )}
+                    </div>
 
-
-{data?.items?.map((item, i) => {
-  return (
-    <div
-      key={i}
-      className={`border border-[#d4e1ff] p-3 mb-3 bg-[linear-gradient(303deg,#2463eb_65%,#abc2f6_102%)] z-[1] rounded-[20px] relative shadow-[0px_0px_10px_1px_#2463eb0f] ${i !== data?.items.length - 1 ? "" : ""}`}
-    >
-       <div className="mb-1">
-  <h3 className="text-sm uppercase tracking-wider font-bold text-[#ffffff] mb-1 text-center">
-    {item?.type}
-  </h3>
-  {item?.description && (
-    <p className="text-white/65 text-sm text-center">
-      {item?.description}
-    </p>
-  )}
-</div>
-
-<div className="text-center">
-  <div className="text-4xl font-bold text-white mb-1">
-    {item?.price}
-    <span className="text-white/65 text-sm">AED</span>
-  </div>
-  <div className="text-white/65 text-sm">/{item?.title}</div>
-</div>
-    </div>
-  );
-})}
-</div>
-<Button className="w-full bg-[#2463eb]  text-white font-medium py-4 rounded-xl mb-6">
-            <Link
-              href="https://calladoc.okadoc.com/en-ae/search/result"
-              target="_blank"
-            >
-              Book Now
-            </Link>
-          </Button>
+                    <div className="text-center">
+                      <div className="text-4xl font-bold text-white mb-1">
+                        {item?.price}
+                        <span className="text-white/65 text-sm">AED</span>
+                      </div>
+                      <div className="text-white/65 text-sm">
+                        /{item?.title}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <Button className="w-full bg-[#2463eb]  text-white font-medium py-4 rounded-xl mb-6">
+              <Link
+                href="https://calladoc.okadoc.com/en-ae/search/result"
+                target="_blank"
+              >
+                Book Now
+              </Link>
+            </Button>
           </div>
-          
+
           <span
             className="size-[600px] block rounded-full absolute left-0 top-0 -translate-x-1/2 -translate-y-1/2  pointer-events-none"
             style={{
@@ -123,19 +124,22 @@ const PackageWidget = ({ data, slug, ...props }) => {
           ></span>
 
           {/* Header with icon and popular badge */}
-       
-         
 
           {/* CTA Button */}
-       
 
           {/* Features section */}
           <div>
-          <h4 className="font-semibold text-gray-900 mb-4">Who can avail</h4>
-          <p className="text-gray-700 text-sm flex-1 mb-7">
-                    {data?.items[0]?.avail||"Type 2 Diabetes, Prediabetes, Gestational Diabetes Mellitus (GDM), Insulin Resistance."}
-                  </p>
-
+            {data?.items[0]?.avail && (
+              <>
+                <h4 className="font-semibold text-gray-900 mb-4">
+                  Who can avail
+                </h4>
+                <p className="text-gray-700 text-sm flex-1 mb-7">
+                  {data?.items[0]?.avail ||
+                    "Type 2 Diabetes, Prediabetes, Gestational Diabetes Mellitus (GDM), Insulin Resistance."}
+                </p>
+              </>
+            )}
             <h4 className="font-semibold text-gray-900 mb-4">Features</h4>
             <div className="space-y-3 mb-7">
               {data?.items[0]?.items?.map((feature, index) => (
@@ -150,9 +154,11 @@ const PackageWidget = ({ data, slug, ...props }) => {
                 </div>
               ))}
             </div>
+            {data?.items[0]?.note && (
             <p className="text-gray-500 text-sm flex-1 ">
-                    {data?.items[0]?.note||"Customization (if required) Lab services screening Medical Assessment devices etc"}
-                  </p>
+              {data?.items[0]?.note ||
+                "Customization (if required) Lab services screening Medical Assessment devices etc"}
+            </p>)}
           </div>
         </Card>
         {/* <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"}>
