12ed2bdf388921c62cd551bfab5a1ca94fa1fed2
bashar8606 <ahammed.bashar9@gmail.com>
2025-10-16 19:47:48 +0400

    dfg

--- src/components/GlobalProviders/index.js
+++ src/components/GlobalProviders/index.js
@@ -1,4 +1,5 @@
 "use client";
+
 import { RecoilRoot } from "recoil";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
@@ -10,16 +11,14 @@ const TawkMessengerReact = dynamic(
   { ssr: false }
 );
 
-const GlobalProviders = ({ children }) => {
+export default function GlobalProviders({ children }) {
   function localStorageProvider() {
     if (typeof window !== "undefined") {
       const map = new Map(JSON.parse(localStorage.getItem("app-cache") || "[]"));
-
       window.addEventListener("beforeunload", () => {
         const appCache = JSON.stringify(Array.from(map.entries()));
         localStorage.setItem("app-cache", appCache);
       });
-
       return map;
     }
   }
@@ -28,9 +27,10 @@ const GlobalProviders = ({ children }) => {
 
   useEffect(() => {
     if (typeof window === "undefined") return;
+
     const enable = () => setIsTawkEnabled(true);
     if ("requestIdleCallback" in window) {
-      // @ts-ignore - safari types
+      // @ts-ignore
       window.requestIdleCallback(enable, { timeout: 3000 });
     } else {
       setTimeout(enable, 2000);
@@ -38,49 +38,35 @@ const GlobalProviders = ({ children }) => {
   }, []);
 
   return (
-    <>
-      <SWRConfig
-        value={{
-          revalidateOnFocus: true,
-          dedupingInterval: 10000,
-          revalidateOnReconnect: false,
-          fetcher: (resource, init) => fetcher(resource, init),
-          ...(typeof window !== "undefined" && {
-            provider: localStorageProvider,
-          }),
-        }}
-      >
-        <RecoilRoot>{children}
-          {isTawkEnabled && (
-            <TawkMessengerReact
-              propertyId="685bb3263e441e1910e8f100"
-              widgetId="1iuj37kfh"
-              onBeforeLoad={() => {
-                console.log("Tawk is about to load");
-              }}
-              onStatusChange={(status) => {
-                console.log("Status changed:", status);
-              }}
-              onChatHidden={() => {
-                console.log("Chat hidden");
-              }}
-              onChatMessageSystem={(message) => {
-                console.log("System message:", message);
-              }}
-              onUnreadCountChanged={(count) => {
-                console.log("Unread count changed:", count);
-              }}
-              onLoad={() => {
-                console.log("Tawk widget loaded");
-              }}
-            />
-          )}
-        </RecoilRoot>
-      </SWRConfig>
+    <SWRConfig
+      value={{
+        revalidateOnFocus: true,
+        dedupingInterval: 10000,
+        revalidateOnReconnect: false,
+        fetcher: (resource, init) => fetcher(resource, init),
+        ...(typeof window !== "undefined" && { provider: localStorageProvider }),
+      }}
+    >
+      <RecoilRoot>
+        {children}
 
-    
-    </>
+        {isTawkEnabled && (
+          <TawkMessengerReact
+            propertyId="685bb3263e441e1910e8f100"
+            widgetId="1iuj37kfh"
+            onLoad={() => console.log("Tawk widget loaded")}
+            onBeforeLoad={() => console.log("Tawk is about to load")}
+            onStatusChange={(status) => console.log("Status changed:", status)}
+            onChatHidden={() => console.log("Chat hidden")}
+            onChatMessageSystem={(message) =>
+              console.log("System message:", message)
+            }
+            onUnreadCountChanged={(count) =>
+              console.log("Unread count changed:", count)
+            }
+          />
+        )}
+      </RecoilRoot>
+    </SWRConfig>
   );
-};
-
-export default GlobalProviders;
+}
