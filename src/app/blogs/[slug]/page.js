3530e446ffd205f0d0b1221421f3a9cf38afcbf2
bashar8606 <ahammed.bashar9@gmail.com>
2025-11-03 22:31:06 +0400

    Update SEO metadata handling in BlogSinglePage and generateMetadata functions; ensure fallback for title and description using data attributes.

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -7,12 +7,17 @@ import Script from "next/script";
 export async function generateMetadata({ params: { slug } }) {
     const data = await getSingleMedia(slug);
     const seo = data?.data?.seo
+    const title=data?.data?.seo?.metaTitle||data?.data?.title
+
+    const desc =seo?.metaDescription ||data?.data?.blocks[0]?.body?.replace(/\r?\n|\r/g, " ")   
+    .replace(/\s+/g, " ")      
+    .trim().slice(0, 150)  ;   
     const adminBaseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://admin.calladoc.ae'
     const schema = {
       "@context": "https://schema.org",
       "@type": "Organization",
-      "name": seo?.metaTitle,
-      "description": seo?.metaDescription,
+      "name": title,
+      "description": desc,
       "url": seo?.url,
       "logo": "https://admin.calladoc.ae/uploads/logo_1_9a17c503ec.svg",
       "contactPoint": {
@@ -23,27 +28,27 @@ export async function generateMetadata({ params: { slug } }) {
     };
     // console.log("meta madia dataaaaaaaaaaaaaaa",data);
     return {
-        title: seo?.metaTitle,
-        description: seo?.metaDescription,
+        title: title,
+        description: desc,
         // keywords: seo?.meta_keywords,
         openGraph: {
-          title: seo?.metaTitle,
-          description: seo?.metaDescription,
+          title: title,
+          description: desc,
           url: seo?.canonicalUrl,
           images: [
             {
               url: seo?.ogImage?.url ? `${adminBaseUrl}${seo.ogImage.url}` : seo?.shareImage?.url ? `${adminBaseUrl}${seo.shareImage.url}` : null,
               width: 1200,
               height: 630,
-              alt: seo?.metaTitle,
+              alt: title,
             },
           ],
           type: seo?.ogType || 'website',
         },
         twitter: {
           card: 'summary_large_image',
-          title: seo?.metaTitle,
-          description: seo?.metaDescription,
+          title: title,
+          description: desc,
           images: [seo?.ogImage?.url ? `${adminBaseUrl}${seo.ogImage.url}` : seo?.shareImage?.url ? `${adminBaseUrl}${seo.shareImage.url}` : null],
         },
         other: {
@@ -56,13 +61,14 @@ export async function generateMetadata({ params: { slug } }) {
 export default async function BlogSinglePage({ params: { slug } }) {
     const data = await getSingleMedia(slug)
     const seo = data?.data?.seo;
+    const title=data?.data?.seo?.metaTitle||data?.data?.title
     
     if (!data) return notFound();
 
     const schemaData = {
       "@context": "https://schema.org",
       "@type": "Organization",
-      "name": seo?.metaTitle,
+      "name": title,
       "description": seo?.metaDescription,
       "url": seo?.url,
       "logo": "https://admin.calladoc.ae/uploads/logo_1_9a17c503ec.svg",
